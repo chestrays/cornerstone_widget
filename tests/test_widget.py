d52b49ce212b91eeef2c7cf805ff8f4b5026a4d6
Kevin Mader <kevinmader@gmail.com>
2018-09-04 23:29:05 +0200

    adding test widget code to make sure state gets updated

---
+++ tests/test_widget.py
@@ -0,0 +1,16 @@
+import numpy as np
+from ipywidgets.embed import embed_snippet
+
+from cornerstone_widget import CornerstoneWidget
+
+
+def test_ipy():
+    c = CornerstoneWidget()
+    c.update_image(np.ones((2, 1)))
+    widget_state = c.get_state()
+    assert widget_state['img_bytes'] == 'AAAAAAAA8D8AAAAAAADwPw=='
+    assert widget_state['img_width'] == 2
+    assert widget_state['img_height'] == 1
+    widget_html = embed_snippet(c)
+    assert 'CornerstoneModel' in widget_html, 'Should contain widget code'
+    assert 'cornerstone_widget' in widget_html, 'Should contain widget code'
