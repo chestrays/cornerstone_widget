171132ec0e5286a40ab8855cb0475b5acac2fed8
Kevin Mader <kmader@users.noreply.github.com>
2018-09-07 11:07:59 +0200

    Update travis_browser.sh
    adding test commands

--- build_tools/travis_browser.sh
+++ build_tools/travis_browser.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+PATH=$PATH:phantomjs-2.1.1-linux-x86_64/bin 
 jupyter notebook --NotebookApp.token='' --no-browser --port 8765
-casperjs tests/test_basic.js
+node_modules/casperjs/bin/casperjs test tests/test_basic.js
